71cb3e7e7fae3e676a379bcf55d5e3092055b205
Aylton Almeida <almeida@aylton.dev>
2022-02-10 00:12:17 -0300

    setup code analyzer

--- Codigo/MobileApp/test/app/modules/delivery_code/views/delivery_code_view_test.dart
+++ Codigo/MobileApp/test/app/modules/delivery_code/views/delivery_code_view_test.dart
@@ -1,18 +1,4 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:delivery_manager/app/modules/delivery_code/controllers/delivery_code_controller.dart';
-import 'package:delivery_manager/app/modules/delivery_code/views/delivery_code_view.dart';
-import 'package:delivery_manager/app/widgets/outlined_text_field.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:get/get.dart';
-
-import '../../../../utils/create_test_widget.dart';
 
 void main() {
   group('Delivery Code View Widget Tests', () {
