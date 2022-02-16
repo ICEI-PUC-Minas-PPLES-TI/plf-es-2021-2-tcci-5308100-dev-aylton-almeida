596f4ad9497a6e5045499d2ecec8008519e4d9a6
Aylton Almeida <almeida@aylton.dev>
2022-02-16 20:19:16 -0300

    add codemagic and remove dead code

--- Codigo/DeliveryApi/src/events/listen/OfferQueue.py
+++
@@ -1,8 +0,0 @@
-from flask import current_app
-
-from src.libs.rabbitmq import rabbit
-
-
-@rabbit.queue(routing_key='*.offer.order.paid')
-def offer_order_paid_queue(routing_key: str, body: dict, message_id: str):
-    current_app.logger.info(body)
