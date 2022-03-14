4e4283dc264cf05740a78d7c50eaed5b4a37128b
Aylton Almeida <almeida@aylton.dev>
2022-03-14 07:51:56 -0300

    added base controller

--- Codigo/DeliveryApi/src/controllers/DeliveryController.py
+++ Codigo/DeliveryApi/src/controllers/DeliveryController.py
@@ -4,8 +4,10 @@ from flask_apispec import MethodResource, doc
 from flask_restful import Resource
 from werkzeug.exceptions import NotFound
 
+from src.classes.Role import Role
 from src.controllers.schemas.DeliveryControllerSchemas import \
     VerifyDeliveryResponseSchema
+from src.guards.AuthGuard import auth_guard
 from src.guards.ExceptionGuard import exception_guard
 from src.guards.MarshalResponse import marshal_response
 from src.services.DeliveryService import DeliveryService
@@ -13,7 +15,7 @@ from src.services.DeliveryService import DeliveryService
 
 class VerifyDeliveryResource(MethodResource, Resource):
 
-    @doc(description="Verifies if delivery with given code exists", tags=['Offer'])
+    @doc(description="Verifies if delivery with given code exists", tags=['Delivery'])
     @exception_guard
     @marshal_response(VerifyDeliveryResponseSchema)
     def get(self, code: str):
@@ -25,3 +27,17 @@ class VerifyDeliveryResource(MethodResource, Resource):
             raise NotFound('Delivery not found with given code')
 
         return {'delivery_id': delivery.delivery_id}, HTTPStatus.OK
+
+
+class DeliveryListResource(MethodResource, Resource):
+
+    @doc(description="Returns all deliveries based on signed in user", tags=['Delivery'])
+    @exception_guard
+    @auth_guard(Role.supplier, needs_user_id=True)
+    @marshal_response(VerifyDeliveryResponseSchema, many=True)
+    def get(self):
+        """Returns all deliveries based on signed in user"""
+
+        deliveries = DeliveryService.get_all()
+
+        return deliveries, HTTPStatus.OK
--- Codigo/DeliveryApi/src/controllers/schemas/DeliveryControllerSchemas.py
+++ Codigo/DeliveryApi/src/controllers/schemas/DeliveryControllerSchemas.py
@@ -3,7 +3,13 @@
 from marshmallow import fields
 
 from src.schemas.CamelCaseSchema import CamelCaseSchema
+from src.schemas.DeliveryBaseSchema import DeliveryBaseSchema
 
 
 class VerifyDeliveryResponseSchema(CamelCaseSchema):
     delivery_id = fields.UUID()
+
+
+class GetSupplierDeliveriesResponseSchema(CamelCaseSchema):
+    deliveries = fields.List(fields.Nested(
+        DeliveryBaseSchema, only=['delivery_id', 'status', ]))
