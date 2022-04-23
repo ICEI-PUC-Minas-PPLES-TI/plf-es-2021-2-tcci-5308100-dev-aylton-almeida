3e27033e05ef6867b7ae0fb29c3c338636fabfde
Aylton Almeida <almeida@aylton.dev>
2022-04-23 19:07:57 -0300

    Teste de E2E da entrega Fixes #103

--- Codigo/MobileApp/integration_test/deliverer_delivery_flow_test.dart
+++ Codigo/MobileApp/integration_test/deliverer_delivery_flow_test.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:delivery_manager/app/data/models/delivery.dart';
 import 'package:delivery_manager/main.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
@@ -35,59 +36,111 @@ Future<void> main() async {
       delivery = Delivery.fromJson(parsed_delivery);
     });
 
-    // testWidgets(
-    //     'See delivery details and start it. Deliver 2 orders and register a problema for the third one, finishing the delivery',
-    //     (WidgetTester tester) async {
-    //   // Initialize app
-    //   await tester.pumpWidget(const MyApp());
-    //   await tester.pumpAndSettle();
+    testWidgets(
+        'See delivery details and start it. Deliver 2 orders and register a problema for the third one, finishing the delivery',
+        (WidgetTester tester) async {
+      // when
+      const orderDetailsCardKey = Key('order_details_card_ink_well');
 
-    //   // sign in supplier
-    //   await testSignInDeliverer(
-    //       tester: tester, deliveryCode: delivery.accessCode!);
-    //   await tester.pumpAndSettle();
+      // Initialize app
+      await tester.pumpWidget(const MyApp());
+      await tester.pumpAndSettle();
 
-    //   // check if the delivery details are showing, including list of products and orders
-    //   expect(find.text('Compra em grupo Charlotte Fifield'), findsOneWidget);
-    //   expect(
-    //     find.text(
-    //       'delivery_initial_address'.tr.replaceAll(
-    //             ':address',
-    //             delivery.orders![0].shippingAddress.formatted,
-    //           ),
-    //     ),
-    //     findsOneWidget,
-    //   );
-    //   expect(
-    //     find.byType(GoogleMap),
-    //     findsOneWidget,
-    //   );
-    //   expect(find.text('start_delivery'.tr), findsOneWidget);
-    //   expect(find.text('cancel_delivery'.tr), findsOneWidget);
-    //   expect(find.text('products'.tr), findsOneWidget);
-    //   expect(find.text('orders'.tr), findsOneWidget);
-    //   expect(
-    //     find.text(delivery.orders![0].orderProducts[0].name),
-    //     findsOneWidget,
-    //   );
+      // sign in supplier
+      await testSignInDeliverer(
+          tester: tester, deliveryCode: delivery.accessCode!);
+      await tester.pumpAndSettle();
 
-    //   // click on the orders tab and expect all orders to be shown
-    //   await tester.tap(find.text('orders'.tr));
-    //   await tester.pumpAndSettle();
-    //   for (final order in delivery.orders!) {
-    //     await tester.tap(find.text('orders'.tr));
-    //     await tester.pumpAndSettle();
-    //     expect(
-    //       find.text(order.buyerName),
-    //       findsOneWidget,
-    //     );
-    //   }
+      // check if the delivery details are showing, including list of products and orders
+      expect(find.text(delivery.name!), findsOneWidget);
+      expect(
+        find.text(
+          'delivery_initial_address'.tr.replaceAll(
+                ':address',
+                delivery.orders![0].shippingAddress.formatted,
+              ),
+        ),
+        findsOneWidget,
+      );
+      expect(find.byType(GoogleMap), findsOneWidget);
+      expect(find.text('start_delivery'.tr), findsOneWidget);
+      expect(find.text('cancel_delivery'.tr), findsOneWidget);
+      expect(find.text('products'.tr), findsOneWidget);
+      expect(find.text('orders'.tr), findsOneWidget);
+      expect(
+        find.text(delivery.orders![0].orderProducts[0].name),
+        findsOneWidget,
+      );
 
-    //   // click on cancel button and check if modal appear
-    //   await tester.tap(find.text('cancel_delivery'.tr));
-    //   await tester.pumpAndSettle();
-    //   expect(find.text('cancel_delivery_modal_title'.tr), findsOneWidget);
-    // });
+      // click on the orders tab and expect all orders to be shown
+      await tester.tap(find.text('orders'.tr));
+      await tester.pumpAndSettle();
+      expect(
+        find.text(delivery.orders![0].buyerName),
+        findsOneWidget,
+      );
+
+      // click on start button and check if dialog appear
+      await tester.tap(find.text('start_delivery'.tr));
+      await tester.pumpAndSettle();
+      expect(find.text('start_delivery_dialog_title'.tr), findsOneWidget);
+
+      // tap confirm button and check if order directions is shown
+      await tester.tap(find.text('confirm'.tr));
+      await tester.pumpAndSettle();
+      expect(find.byType(GoogleMap), findsOneWidget);
+
+      // deliver first order
+      await tester.tap(find.byKey(orderDetailsCardKey));
+      await tester.pumpAndSettle();
+      expect(find.text('next_delivery'.tr), findsOneWidget);
+      await tester.tap(find.text('view_details'.tr));
+      await tester.pumpAndSettle();
+      expect(find.text('order_details'.tr), findsOneWidget);
+      await tester.tap(find.text('confirm_delivery'.tr));
+      await tester.pumpAndSettle();
+      expect(find.text('delivery_confirmed'.tr), findsOneWidget);
+
+      // register problem for second one
+      await tester.tap(find.byKey(orderDetailsCardKey));
+      await tester.pumpAndSettle();
+      await tester
+          .pump(const Duration(seconds: 5)); // Wait snackbar to disappear
+      expect(find.text('next_delivery'.tr), findsOneWidget);
+      await tester.tap(find.text('view_details'.tr));
+      await tester.pumpAndSettle();
+      expect(find.text('order_details'.tr), findsOneWidget);
+      await tester.tap(find.text('register_problem'.tr));
+      await tester.pumpAndSettle();
+      expect(find.text('order_problem_header'.tr), findsOneWidget);
+      expect(find.text('order_problem_subheader'.tr), findsOneWidget);
+      await tester.tap(find.text('order_problem_select_hint'.tr));
+      await tester.pumpAndSettle();
+      await tester.tap(find.text('absent_receiver'.tr).first);
+      await tester.pumpAndSettle();
+      await tester.tap(find.text('send_problem'.tr));
+      await tester.pumpAndSettle();
+      expect(find.text('problem_registered'.tr), findsOneWidget);
+
+      // deliver third one
+      await tester.tap(find.byKey(orderDetailsCardKey));
+      await tester.pumpAndSettle();
+      await tester
+          .pump(const Duration(seconds: 5)); // Wait snackbar to disappear
+      expect(find.text('next_delivery'.tr), findsOneWidget);
+      await tester.tap(find.text('view_details'.tr));
+      await tester.pumpAndSettle();
+      expect(find.text('order_details'.tr), findsOneWidget);
+      await tester.tap(find.text('confirm_delivery'.tr));
+      await tester.pumpAndSettle();
+      expect(find.text('delivery_confirmed'.tr), findsOneWidget);
+
+      // expect thank you and going back to start
+      expect(find.text('delivery_complete_header'.tr), findsOneWidget);
+      await tester.tap(find.text('go_back_to_start'.tr));
+      await tester.pumpAndSettle();
+      expect(find.text('delivery_code_form_header'.tr), findsOneWidget);
+    });
 
     testWidgets('See delivery details and cancel it.',
         (WidgetTester tester) async {
@@ -101,7 +154,7 @@ Future<void> main() async {
       await tester.pumpAndSettle();
 
       // check if the delivery details are showing, including list of products and orders
-      expect(find.text('Compra em grupo Charlotte Fifield'), findsOneWidget);
+      expect(find.text(delivery.name!), findsOneWidget);
       expect(
         find.text(
           'delivery_initial_address'.tr.replaceAll(
@@ -111,10 +164,7 @@ Future<void> main() async {
         ),
         findsOneWidget,
       );
-      expect(
-        find.byType(GoogleMap),
-        findsOneWidget,
-      );
+      expect(find.byType(GoogleMap), findsOneWidget);
       expect(find.text('start_delivery'.tr), findsOneWidget);
       expect(find.text('cancel_delivery'.tr), findsOneWidget);
       expect(find.text('products'.tr), findsOneWidget);
@@ -127,19 +177,20 @@ Future<void> main() async {
       // click on the orders tab and expect all orders to be shown
       await tester.tap(find.text('orders'.tr));
       await tester.pumpAndSettle();
-      for (final order in delivery.orders!) {
-        await tester.tap(find.text('orders'.tr));
-        await tester.pumpAndSettle();
-        expect(
-          find.text(order.buyerName),
-          findsOneWidget,
-        );
-      }
+      expect(
+        find.text(delivery.orders![0].buyerName),
+        findsOneWidget,
+      );
 
-      // click on cancel button and check if modal appear
+      // click on cancel button and check if dialog appear
       await tester.tap(find.text('cancel_delivery'.tr));
       await tester.pumpAndSettle();
-      expect(find.text('cancel_delivery_modal_title'.tr), findsOneWidget);
+      expect(find.text('cancel_delivery_dialog_title'.tr), findsOneWidget);
+
+      // tap confirm button and check if returned to login screen
+      await tester.tap(find.text('confirm'.tr));
+      await tester.pumpAndSettle();
+      expect(find.text('delivery_code_form_header'.tr), findsOneWidget);
     });
   });
 }
