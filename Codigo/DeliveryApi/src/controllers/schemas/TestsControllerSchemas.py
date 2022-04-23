4b11bda266b824d4332129d7bdd311c8e47a4e88
Aylton Almeida <almeida@aylton.dev>
2022-04-23 17:43:48 -0300

    update integration test schema

---
+++ Codigo/DeliveryApi/src/controllers/schemas/TestsControllerSchemas.py
@@ -0,0 +1,6 @@
+from src.schemas.CamelCaseSchema import CamelCaseSchema
+from src.schemas.DeliveryBaseSchema import DeliveryBaseSchema
+
+
+class SetupDeliveryResponseSchema(CamelCaseSchema, DeliveryBaseSchema):
+    pass
